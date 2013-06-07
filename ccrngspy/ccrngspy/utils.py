683736ba5654d83319e14bb33445748633ef9df7
Kenneth Daily <dailykm@mail.nih.gov>
2013-06-07 17:22:43 -0400

    Updated safe run format.

--- ccrngspy/ccrngspy/utils.py
+++ ccrngspy/ccrngspy/utils.py
@@ -3,10 +3,12 @@ import shlex
 import logging
 import tempfile
 import time
+import os
+import sys
 
 def safe_run(cmd, shell=False):
     proc = None
-
+    
     if (isinstance(cmd, list)):
         proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     else:
@@ -14,7 +16,7 @@ def safe_run(cmd, shell=False):
             proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         else:
             proc = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
+            
     while True:
         std_output = proc.stdout.readline()
         print std_output.rstrip()
@@ -54,13 +56,13 @@ def safe_qsub_run(cmd, jobname, script_header=_script_header, nodes=1, params=""
     
     qsub_cmd = "%(cmd)s %(script)s" % dict(cmd=qsub_cmd, script=scriptfile.name)
 
-    # proc = subprocess.Popen(qsub_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # so, se = proc.communicate()
+    proc = subprocess.Popen(shlex.split(qsub_cmd), shell=False, 
+                            stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
 
-    proc = subprocess.Popen(shlex.split(qsub_cmd), shell=False, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+
     stdouts, stderrs = proc.communicate()
-
     jobid = stdouts.rstrip()
+
     scriptfile.close()
 
     return jobid#, stderrs
@@ -75,9 +77,16 @@ _LOGGING_COLOR = {'yellow' : '\033[93m',
                   'green' : '\033[92m',
                   'blue' : '\033[94m',
                   'red' : '\033[91m',
+                  'bold_red': "\033[1;31m",
                   'grey' : '\033[90m',
                   True : '\033[90m',
                   False : '\033[90m',}
+
+_LOGGER_LOGGING_COLOR = {'debug': _LOGGING_COLOR['blue'],
+                         'info': _LOGGING_COLOR['green'],
+                         'warning': _LOGGING_COLOR['yellow'],
+                         'error': _LOGGING_COLOR['red'],
+                         'critical':  _LOGGING_COLOR['bold_red']}
 
 def make_local_logger(logger_name, level="info", color=False):
     """Helper function to make local loggers with color.
@@ -91,7 +100,7 @@ def make_local_logger(logger_name, level="info", color=False):
     except KeyError:
         logger.setLevel(level)
     
-    format = "%(asctime)s - %(name)s - " + _LOGGING_COLOR[color] + "%(levelname)s:%(module)s.%(funcName)s\033[0m - %(message)s"
+    format = "%(asctime)s - %(name)s - " + _LOGGER_LOGGING_COLOR[level] + "%(levelname)s:%(module)s.%(funcName)s\033[0m - %(message)s"
         
     formatter = logging.Formatter(format)
 
