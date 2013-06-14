59be4bc7b315d7b236efac6623f3bdb79cc73fd8
Kenneth Daily <dailykm@mail.nih.gov>
2013-06-14 10:48:33 -0400

    Added cleansam step.

--- scripts/exome_khanbwa_qsub.py
+++ scripts/exome_khanbwa_qsub.py
@@ -255,6 +255,14 @@ def run_indexbam1(input, output, params=None):
     
     logger.debug("job_id = %s" % (job_id,))
 
+@follows(run_indexbam1)
+@transform(run_filterbam, regex(r"(.*).bam"), r"\1.clean.bam")
+def run_cleansam(input, output):
+    """Clean up BAM file.
+    """
+    
+    cmd = "java -Xmx%(maxjheap)s -Djava.io.tmpdir=%(tmp_dir)s -jar %(jar_file)s INPUT=%(input)s OUTPUT=%(output)s"
+
 job_list = [run_mk_output_dir, run_bwa_aln, run_bwa_sampe, run_indexbam1]
 
 def run_it():
